743a755872cc937ef676dd9fa463673d2c0c2511
mjbdev <maximiliano.borda@gmail.com>
2023-08-08 20:50:29 -0300

    Remove unnecessary comments

--- src/main/java/com/globallogic/users/UsersApplication.java
+++ src/main/java/com/globallogic/users/UsersApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class UsersApplication {
-
-	// van 5 , a las 15:00 hs
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-
 }
